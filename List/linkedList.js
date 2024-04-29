e556aaa2b74459f098a6b9ce32d5b00e6dd4bec6
caioslopes <caiolopes.social@gmail.com>
2024-04-28 22:07:11 -0300

    LinkedList and Node Class

---
+++ List/linkedList.js
@@ -0,0 +1,117 @@
+import { defaultEquals } from "../utils/utils";
+import { Node } from "./node";
+
+export default class LinkedList {
+  constructor(equalsFn = defaultEquals) {
+    this.count = 0;
+    this.head = undefined;
+    this.equalsFn = equalsFn;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  push(element) {
+    const node = new Node(element);
+    let current;
+
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      current = this.head;
+
+      while (current.next != null) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+
+    this.count++;
+  }
+
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new Node(element);
+      if (index === 0) {
+        const current = this.head;
+        node.next = current;
+        this.head = node;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        const current = previous.next;
+        node.next = current;
+        previous.next = node;
+      }
+      this.count++;
+      return true;
+    }
+    return false;
+  }
+
+  indexOf(element) {
+    let current = this.head;
+    for (let i = 0; i < this.count && current !== null; i++) {
+      if (this.equalsFn(element, current.element)) {
+        return i;
+      }
+      current = current.next;
+    }
+    return -1;
+  }
+
+  getElementAt(index) {
+    if (index >= 0 && index <= this.count) {
+      let current = this.head;
+      for (let i = 0; i < index && current !== null; i++) {
+        current = current.next;
+      }
+      return current;
+    }
+    return undefined;
+  }
+
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        previous.next = current.next;
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
+  }
+
+  remove(element) {
+    const index = this.indexOf(element);
+    return this.removeAt(index);
+  }
+
+  toString() {
+    if (this.head == null) {
+      return "";
+    }
+    let objString = `${this.head.element}`;
+    let current = this.head.next;
+    for (let i = 1; i < this.size() && current !== null; i++) {
+      objString = `${objString},${current.element}`;
+      current = current.next;
+    }
+    return objString;
+  }
+}
