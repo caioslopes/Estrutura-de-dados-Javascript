0dd163f36df261b565663be11f2583e56474235b
caioslopes <caiolopes.social@gmail.com>
2024-05-19 19:24:15 -0300

    add: doing recursion

--- DoingRecursion/doingRecursion.js
+++ DoingRecursion/doingRecursion.js
@@ -8,4 +8,31 @@ function factorialIterative(number) {
   return total;
 }
 
+// Factorial of a number with recursive function
+function factorial(n) {
+  if (n === 1 || n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+function fibonacci(n) {
+  if (n < 1) return 0;
+  if (n <= 2) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function fibonacciMemoization(n) {
+  const memo = [0, 1];
+  const fibonacci = (n) => {
+    if (memo[n] != null) return memo[n];
+    return (memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo));
+  };
+  return fibonacci(n);
+}
+
+// Call functions
 console.log(factorialIterative(5));
+console.log(factorial(5));
+console.log(fibonacci(8));
+console.log(fibonacciMemoization(8));
